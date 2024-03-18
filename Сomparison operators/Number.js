6def367cc9c28876a13d51e004b7dc3a4318d755
CarolineSouth <58517954+CarolineSouth@users.noreply.github.com>
2024-03-18 12:23:05 +0100

    Added 2.js

--- Сomparison operators/Number.js
+++ Сomparison operators/Number.js
@@ -1,12 +1,12 @@
 let a = 1;
 let b = 5;
-b==a;
+//b==a;
 console.log(a)
 let c = 10;
 console.log(c<b);
 console.log(b>c);
-console.log(a!=c);
+//console.log(a!=c);
 let d = 14;
-console.log(d!=a);
+//console.log(d!=a);
 console.log(a>=a, d<=a);
 
