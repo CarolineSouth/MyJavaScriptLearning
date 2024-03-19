4fdb8fb9a5ddd3dbe9842b027764b05b8d4db330
CarolineSouth <58517954+CarolineSouth@users.noreply.github.com>
2024-03-19 22:40:51 +0100

    Added ifeLSE.JS

--- IfElse/IfElse.js
+++ IfElse/IfElse.js
@@ -1,9 +1,43 @@
-let number = 10;
-if (number > 0){
-    console.log("The number is positive");
+// let number = 10;
+// if (number > 0){
+//     console.log("The number is positive");
 
-} else if (number < 0){
-    console.log("The number is negative"); 
+// } else if (number < 0){
+//     console.log("The number is negative"); 
+// } else {
+//     console.log("The number is zero")
+// }
+
+//let year = 18;
+// prompt('How old are you?');
+// if (year <= 18){
+//     alert('Your in!');
+// } else {
+//     alert ("Your out!")
+// }
+
+//Write a JavaScript program that checks if a given number is positive, negative, or zero.
+//You need to convert the user input (which is a string) to a number using the parseInt() function
+let number =parseInt(prompt ('Enter the number'));
+
+if ( number > 0){
+    alert(number +' it is positive');
+} else if ( number < 0){
+    alert(number + ' it is negative');
 } else {
-    console.log("The number is zero")
+    alert(number + ' it is 0')
+}
+
+//Create a function that takes two numbers as parameters and returns the larger of the two.
+let number1;
+let number2;
+
+function TwoParametrs(number1,number2){
+number1 = parseInt(prompt ('Enter Number one'));
+number2 = parseInt(prompt('Enter number two'));
+if (number1>number2){
+    alert(number1 + " is larger");
+} else {
+    alert(number2 + " is larger")
+}
 }
