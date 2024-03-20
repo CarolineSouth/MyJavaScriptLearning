46a554491d3c6aaebd6972f384333240bd19feae
CarolineSouth <58517954+CarolineSouth@users.noreply.github.com>
2024-03-20 15:03:38 +0100

    Added ifElse3.js

--- IfElse/IfElse.js
+++ IfElse/IfElse.js
@@ -1,12 +1,12 @@
-// let number = 10;
-// if (number > 0){
-//     console.log("The number is positive");
+let number = 15;
+if (number > 0){
+    console.log("The number is positive");
 
-// } else if (number < 0){
-//     console.log("The number is negative"); 
-// } else {
-//     console.log("The number is zero")
-// }
+} else if (number < 0){
+    console.log("The number is negative"); 
+} else {
+    console.log("The number is zero")
+}
 
 //let year = 18;
 // prompt('How old are you?');
@@ -16,38 +16,61 @@
 //     alert ("Your out!")
 // }
 
-//Write a JavaScript program that checks if a given number is positive, negative, or zero.
-//You need to convert the user input (which is a string) to a number using the parseInt() function
-let number =parseInt(prompt ('Enter the number'));
+// //Write a JavaScript program that checks if a given number is positive, negative, or zero.
+// //You need to convert the user input (which is a string) to a number using the parseInt() function
+// let number =parseInt(prompt ('Enter the number'));
 
-if ( number > 0){
-    alert(number +' it is positive');
-} else if ( number < 0){
-    alert(number + ' it is negative');
-} else {
-    alert(number + ' it is 0')
-}
+// if ( number > 0){
+//     alert(number +' it is positive');
+// } else if ( number < 0){
+//     alert(number + ' it is negative');
+// } else {
+//     alert(number + ' it is 0')
+// }
 
-//Create a function that takes two numbers as parameters and returns the larger of the two.
-let number1;
-let number2;
+// //Create a function that takes two numbers as parameters and returns the larger of the two.
+// let number1;
+// let number2;
 
-function TwoParametrs(){
-number1 = parseInt(prompt ('Enter Number one'));
-number2 = parseInt(prompt('Enter number two'));
-if (number1>number2){
-    alert(number1 + " is larger");
-} else {
-    alert(number2 + " is larger")
-}
-}
-console.log(TwoParametrs());
+// function TwoParametrs(){
+// number1 = parseInt(prompt ('Enter Number one'));
+// number2 = parseInt(prompt('Enter number two'));
+// if (number1>number2){
+//     alert(number1 + " is larger");
+// } else {
+//     alert(number2 + " is larger")
+// }
+// }
+// console.log(TwoParametrs());
 
-// Write a program that checks whether a given year is a leap year or not.
-let year = parseInt(prompt('Enter year'));
-if (year%4 ===0 ){
-    alert('This year is leap')
+// // Write a program that checks whether a given year is a leap year or not.
+// let year = parseInt(prompt('Enter year'));
+// if (year%4 ===0 ){
+//     alert('This year is leap')
 
-} else {
-    alert('This year is NOT leap')
-}
+// } else {
+//     alert('This year is NOT leap')
+// }
+
+// //Develop a function that takes a string as input and returns true if the string has more than 5 characters, otherwise returns false.
+
+// let word;
+// function takesString(){
+//     word = prompt('Enter word or phrase');
+//     if (word.length >= 5){
+//         alert('True');
+//     } else {
+//         alert('False');
+//     }
+// }
+// console.log(takesString());
+
+// // Create a program that determines if a given number is even or odd.
+// let number3 = parseInt(prompt('Enter number'));
+// if (number3%2 ===0){
+//     alert(number3 + ' This number is EVEN')
+// } else {
+//     alert(number3 + ' This number is ODD')
+// }
+
+
